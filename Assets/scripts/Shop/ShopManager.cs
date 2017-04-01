06277b98e6ba2eb84458dcb72f14429e2a5c18e0
EQwon <bluetwintail@naver.com>
2017-04-01 23:56:02 +0900

    버튼 클릭하면 해당하는 정보가 나오도록 함

---
+++ Assets/scripts/Shop/ShopManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopManager : MonoBehaviour
+{
+	static int money;
+	public static ShopManager Instance;
+
+	public Image Name;
+	public Image BigIcon;
+	public Image Price;
+	public Image Information;
+	public Text MoneyText;
+
+	public List<Upgrade> Upgrades;
+
+	private void Awake()
+	{
+		Instance = this;
+	}
+
+	private void Update()
+	{
+		MoneyText.text = "$ " + money.ToString();
+	}
+
+	public void ClickedUpgradeButton(int which) //which : "Which" did you clicked? 
+	{
+		Name.sprite = Upgrades[which].UpgradeName;
+		BigIcon.sprite = Upgrades[which].UpgradeBigIcon;
+		Information.sprite = Upgrades[which].UpgradeInfo;
+	}
+
+	public void CheatButton()
+	{
+		money = money + 1000;
+	}
+
+	[System.Serializable]
+	public class Upgrade
+	{
+		public Sprite UpgradeBigIcon;
+		public Sprite UpgradeName;
+		public Sprite UpgradeInfo;
+	}
+}
