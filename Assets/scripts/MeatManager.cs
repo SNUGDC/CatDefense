a3cd8a4f258d18bc2b3f806bc9fd9da60b72137e
Juhyeong Park <majecty@gmail.com>
2017-04-01 18:21:49 +0900

    Initially no meat

--- Assets/scripts/MeatManager.cs
+++ Assets/scripts/MeatManager.cs
@@ -1,41 +1,74 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MeatManager : MonoBehaviour
 {
-	public MeatSelection meatSelection;
+    public MeatSelection meatSelection;
+    public MeatOnDoma meatOnDoma;
+    private MeatSpecies? _meatSpecies;
+    private MeatSpecies? meatSpecies
+    {
+        get
+        {
+            return _meatSpecies;
+        }
+        set
+        {
+            _meatSpecies = value;
+            meatSelection.meatSpecies = value;
+            if (_meatSpecies == null)
+            {
+                meatOnDoma.HideMeat();
+            }
+            else
+            {
+                meatOnDoma.ShowMeat();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        meatSpecies = null;
+    }
 
     void Update()
     {
-		MeatSpecies? selectedMeat = SelectMeat();
-		if (selectedMeat != null) {
-			meatSelection.meatSpecies = selectedMeat;
-		}
+        MeatSpecies? selectedMeat = SelectMeat();
+        if (selectedMeat != null)
+        {
+            meatSpecies = selectedMeat;
+        }
     }
 
     private static MeatSpecies? SelectMeat()
     {
         if (Input.GetKeyUp(KeyCode.Q))
         {
-			return MeatSpecies.Sheep;
+            return MeatSpecies.Sheep;
         }
         else if (Input.GetKeyUp(KeyCode.E))
         {
-			return MeatSpecies.Duck;
+            return MeatSpecies.Duck;
         }
         else if (Input.GetKeyUp(KeyCode.A))
         {
-			return MeatSpecies.Salmon;
+            return MeatSpecies.Salmon;
         }
         else if (Input.GetKeyUp(KeyCode.S))
         {
-			return MeatSpecies.Cow;
+            return MeatSpecies.Cow;
         }
         else if (Input.GetKeyUp(KeyCode.D))
         {
-			return MeatSpecies.Chicken;
+            return MeatSpecies.Chicken;
         }
-		return null;
+        return null;
     }
 }
---
+++ Assets/scripts/MeatOnDoma.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeatOnDoma : MonoBehaviour {
+
+	public SpriteRenderer spriteRenderer;
+
+	public void ShowMeat()
+	{
+		spriteRenderer.enabled = true;
+	}
+
+	public void HideMeat()
+	{
+		spriteRenderer.enabled = false;
+	}
+}
