04aa67dd6af6dbe15589826f53ae4aca7dada249
Juhyeong Park <majecty@gmail.com>
2017-04-01 21:51:14 +0900

    Spawn cat image

--- Assets/scripts/meatShooter/CatSpawner.cs
+++ Assets/scripts/meatShooter/CatSpawner.cs
@@ -7,6 +7,7 @@ public class CatSpawner : MonoBehaviour
 {
     public Transform left;
     public Transform right;
+	public Cat catPrefab;
 
     /// <summary>
     /// Start is called on the frame when a script is enabled just before
@@ -35,6 +36,8 @@ public class CatSpawner : MonoBehaviour
 
     private void SpawnCat()
     {
-        Debug.Log("Cat spawned");
+        Cat cat = Instantiate(catPrefab) as Cat;
+		float x = UnityEngine.Random.Range(left.position.x, right.position.x);
+		cat.transform.position = new Vector3(x, transform.position.y, transform.position.z);
     }
 }
