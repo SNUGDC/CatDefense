d8ce452419106e8eb9aac1321d62f3f727676185
EQwon <bluetwintail@naver.com>
2017-04-02 01:56:10 +0900

    아트 리소스 추가 & 상점 구현 거의 완료

---
+++ Assets/scripts/GlobalInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalInfo
+{
+    private static GlobalInfo _instance;
+    public static GlobalInfo Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new GlobalInfo();
+            }
+            return _instance;
+        }
+    }
+
+	public GlobalInfo()
+	{
+		money = 0;
+		UpgradeLevel = new List<int> {
+			0, 0, 0, 0, 0, 0
+		};
+	}
+
+    public int money;
+    public List<int> UpgradeLevel;
+
+}
