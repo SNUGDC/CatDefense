972b634e3c0f4212d713319602590c42b0cb1811
Juhyeong Park <majecty@gmail.com>
2017-04-02 18:59:28 +0900

    Make game end screen working

---
+++ Assets/scripts/InGameCommon/ClearScreen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearScreen : MonoBehaviour {
+
+	public void GoToShop()
+	{
+		BackgroundMusic.FadeOutAndChangeScene("Shop");
+	}
+
+	public void GoToNext()
+	{
+		BackgroundMusic.FadeOutAndChangeScene("Main");
+	}
+}
---
+++ Assets/scripts/InGameCommon/FailScreen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FailScreen : MonoBehaviour {
+
+	public void GoToRetry()
+	{
+		BackgroundMusic.FadeOutAndChangeScene("Main");
+	}
+
+	public void GoToShop()
+	{
+		BackgroundMusic.FadeOutAndChangeScene("Shop");
+	}
+}
