f1f6ecf1a49084b9595b51de951fe7301b6ebea8
Juhyeong Park <majecty@gmail.com>
2017-04-02 12:18:33 +0900

    Before rename cutting result

---
+++ Assets/scripts/meatCutter/JudgementPopup.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JudgementPopup : MonoBehaviour
+{
+    public Sprite perfect;
+    public Sprite good;
+    public Sprite bad;
+	private IEnumerator banishCoroutine;
+
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	void Awake()
+	{
+		GetComponent<SpriteRenderer>().sprite = null;
+	}
+    public void SetJudgementVoid(CuttingJudgement judgement)
+    {
+		if (banishCoroutine != null)
+		{
+			StopCoroutine(banishCoroutine);
+		}
+
+        Sprite sprite = GetSprite(judgement);
+		GetComponent<SpriteRenderer>().sprite = sprite;
+		banishCoroutine = BanishCoroutine();
+		StartCoroutine(banishCoroutine);
+    }
+
+	private IEnumerator BanishCoroutine()
+	{
+		yield return new WaitForSeconds(0.2f);
+		
+		GetComponent<SpriteRenderer>().sprite = null;
+		banishCoroutine = null;
+	}
+
+    private Sprite GetSprite(CuttingJudgement judgement)
+    {
+        switch (judgement)
+        {
+            case CuttingJudgement.Perfect:
+                return perfect;
+            case CuttingJudgement.Good:
+                return good;
+            case CuttingJudgement.Bad:
+                return bad;
+        }
+
+        return null;
+    }
+}
