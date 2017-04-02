95b3386c7cd390b0cdd2b68713c7f25c17809098
Juhyeong Park <majecty@gmail.com>
2017-04-02 15:21:28 +0900

    Apply knife speed upgrade

---
+++ Assets/scripts/UpgradeApplier.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeApplier : MonoBehaviour {
+	public static UpgradeApplier Instance;
+
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	void Awake()
+	{
+		Instance = this;
+	}
+	public float GetKnifeSpeedMultiplier()
+	{
+		return 1.0f + 0.2f * (float)GlobalInfo.Instance.GetKnifeUpgradeLevel();
+	}
+}
--- Assets/scripts/meatCutter/ManualKnifeMoving.cs
+++ Assets/scripts/meatCutter/ManualKnifeMoving.cs
@@ -6,13 +6,15 @@ public class ManualKnifeMoving : MonoBehaviour
 {
 	private void Update()
 	{
+		float defaultSpeed = Configurations.Instance.defaultKnifeSpeed;
+		float speed = defaultSpeed * UpgradeApplier.Instance.GetKnifeSpeedMultiplier();
 		if(Input.GetKey(KeyCode.C))
 		{
-			transform.position = new Vector3(transform.position.x + 0.1f, transform.position.y, transform.position.z);
+			transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
 		}
 		else if(Input.GetKey(KeyCode.Z))
 		{
-			transform.position = new Vector3(transform.position.x - 0.1f, transform.position.y, transform.position.z);
+			transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
 		}
 	}
 }
