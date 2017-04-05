224c41376d5fcb177aea565e547ae7b8ab20b571
Juhyeong Park <majecty@gmail.com>
2017-04-05 18:51:54 +0900

    Add tutorial

--- Assets/scripts/MoveSceneFromStart.cs
+++ Assets/scripts/MoveSceneFromStart.cs
@@ -7,6 +7,6 @@ public class MoveSceneFromStart : MonoBehaviour
 {
 	public void ClickStartButton()
 	{
-		SceneManager.LoadScene("Main");
+		SceneManager.LoadScene("Tutorial");
 	}
 }
---
+++ Assets/scripts/Tutorial.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Tutorial : MonoBehaviour {
+
+	public List<GameObject> tutorials;
+	/// <summary>
+	/// Start is called on the frame when a script is enabled just before
+	/// any of the Update methods is called the first time.
+	/// </summary>
+	void Start()
+	{
+		StartCoroutine(TutorialCoroutine());
+	}
+
+	IEnumerator TutorialCoroutine()
+	{
+		foreach (var tutorial in tutorials)
+		{
+			tutorials.ForEach(t => t.SetActive(false));
+			tutorial.SetActive(true);
+			yield return new WaitForSeconds(1.5f);
+		}
+
+		SceneManager.LoadScene("Main");
+	}
+}
