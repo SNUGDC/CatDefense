12423f9dfe75857957d4edc2a506ebb1d0be6b70
EQwon <bluetwintail@naver.com>
2017-04-05 20:27:26 +0900

    오프닝 씬 추가

---
+++ Assets/scripts/Opening.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Opening : MonoBehaviour
+{
+    public SpriteRenderer LogoIcon1;
+    public SpriteRenderer LogoIcon2;
+    public SpriteRenderer LogoIcon3;
+    public SpriteRenderer LogoIcon4;
+    public SpriteRenderer LogoFace;
+    public SpriteRenderer LogoWord1;
+    public SpriteRenderer LogoWord2;
+    public SpriteRenderer LogoWord3;
+    public SpriteRenderer LogoName;
+    public GameObject Panel;
+
+    private float OpeningTime;
+    private bool OpeningOnGoing;
+
+    void Start()
+    {
+        OpeningOnGoing = true;
+
+        LogoIcon1.color = new Color(1, 1, 1, 0);
+        LogoIcon2.color = new Color(1, 1, 1, 0);
+        LogoIcon3.color = new Color(1, 1, 1, 0);
+        LogoIcon4.color = new Color(1, 1, 1, 0);
+        LogoFace.color = new Color(1, 1, 1, 0);
+        LogoWord1.color = new Color(1, 1, 1, 0);
+        LogoWord2.color = new Color(1, 1, 1, 0);
+        LogoWord3.color = new Color(1, 1, 1, 0);
+        LogoName.color = new Color(1, 1, 1, 0);
+
+        OpeningTime = 0;
+    }
+
+    void Update()
+    {
+        OpeningTime = OpeningTime + Time.deltaTime;
+
+        if (OpeningTime >= 6f)
+            OpeningOnGoing = false;
+
+        if (OpeningOnGoing == true)
+        {
+            LogoFace.color = new Color(1, 1, 1, OpeningTime);
+
+            if (OpeningTime >= 1f)
+            {
+                LogoIcon1.color = new Color(1, 1, 1, 1);
+            }
+            if (OpeningTime >= 1.5f)
+            {
+                LogoIcon2.color = new Color(1, 1, 1, 1);
+            }
+            if (OpeningTime >= 2f)
+            {
+                LogoIcon3.color = new Color(1, 1, 1, 1);
+            }
+            if (OpeningTime >= 2.5f)
+            {
+                LogoIcon4.color = new Color(1, 1, 1, 1);
+            }
+            if (OpeningTime >= 3f)
+            {
+                LogoWord1.color = new Color(1, 1, 1, OpeningTime - 3f);
+            }
+            if (OpeningTime >= 3.5f)
+            {
+                LogoWord2.color = new Color(1, 1, 1, OpeningTime - 3.5f);
+            }
+            if (OpeningTime >= 4f)
+            {
+                LogoWord3.color = new Color(1, 1, 1, OpeningTime - 4f);
+            }
+            if (OpeningTime >= 4.5f)
+            {
+                Panel.GetComponent<Transform>().position = new Vector3(Panel.GetComponent<Transform>().position.x + (OpeningTime / 60), Panel.GetComponent<Transform>().position.y, 0);
+                LogoName.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, (OpeningTime / 1.5f) - 3f);
+            }
+        }
+        else
+        {
+            LogoIcon1.color = new Color(1, 1, 1, 1);
+            LogoIcon2.color = new Color(1, 1, 1, 1);
+            LogoIcon3.color = new Color(1, 1, 1, 1);
+            LogoIcon4.color = new Color(1, 1, 1, 1);
+            LogoFace.color = new Color(1, 1, 1, 1);
+            LogoWord1.color = new Color(1, 1, 1, 1);
+            LogoWord2.color = new Color(1, 1, 1, 1);
+            LogoWord3.color = new Color(1, 1, 1, 1);
+            LogoName.color = new Color(1, 1, 1, 1);
+            Panel.SetActive(false);
+        }
+    }
+
+    public void SkipLogo()
+    {
+        if (OpeningOnGoing == true)
+        {
+            OpeningOnGoing = false;
+        }
+        else
+        {
+            SceneManager.LoadScene("Start");
+        }
+    }
+}
