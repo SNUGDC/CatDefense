f7b5755c4e00a6cc55ed56536cccb3720da0a27b
EQwon <bluetwintail@naver.com>
2017-04-11 19:48:47 +0900

    튜토리얼 한 장면당 유지시간 3초에서 5초로 증가

--- Assets/scripts/Tutorial.cs
+++ Assets/scripts/Tutorial.cs
@@ -20,7 +20,7 @@ public class Tutorial : MonoBehaviour {
 		{
 			tutorials.ForEach(t => t.SetActive(false));
 			tutorial.SetActive(true);
-			yield return new WaitForSeconds(3f);
+			yield return new WaitForSeconds(5f);
 		}
 
 		SceneManager.LoadScene("Main");
